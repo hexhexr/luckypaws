7c4d85efccfed187e53ce76557c78f3238bb19ee
hexhexr <digdaguu@gmail.com>
2025-05-18 05:48:24 +0500

    zmhfds

--- pages/api/check-status.js
+++ pages/api/check-status.js
@@ -1,4 +1,5 @@
-import { db } from '../../../lib/firebaseAdmin';
+import { db } from '../../lib/firebaseAdmin';
+
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
