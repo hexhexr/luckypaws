f14047ebbc5bded89fd7ed5abf586219e4b52b87
hexhexr <digdaguu@gmail.com>
2025-06-09 01:11:28 +0500

    fix

--- src/pages/api/admin/agents/[id].js
+++ src/pages/api/admin/agents/[id].js
@@ -1,6 +1,6 @@
 // pages/api/admin/agents/[id].js
 import { getFirestore } from "firebase-admin/firestore";
-import { firebaseAdmin } from "../../../../../lib/firebaseAdmin";
+import { firebaseAdmin } from '../../../lib/firebaseAdmin';
 
 const db = getFirestore(firebaseAdmin);
 
